e3667ee4097c5ce106f1a7d013771e2d62df05a5
Derek Huang <derekh919@gmail.com>
2020-11-29 12:27:57 -0500

    setup tailwind

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     'gatsby-plugin-less',
+    'gatsby-plugin-postcss',
     {
       resolve: 'gatsby-plugin-purgecss',
       options: {
